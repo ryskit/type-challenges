6729f67706366d36fcffd5d897b54b575ec7abc5
ryskit <ryosukekida.71@gmail.com>
2026-02-01 00:57:03 +0900

    resolved 00008 medium readonly 2

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,5 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [Q in keyof T as Q extends K ? Q : never]: T[Q]
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
